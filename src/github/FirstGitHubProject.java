d247bde9ae88a8a308d8de801db16e4107d22430
Precupetu <aprecupetu@ea.com>
2016-04-11 17:39:02 +0300

    ToDoList

--- src/github/FirstGitHubProject.java
+++ src/github/FirstGitHubProject.java
@@ -5,6 +5,7 @@ public class FirstGitHubProject {
 	public static void main(String[] args) {
 
 		System.out.println("Linia adaugata de Stingster!");
+		System.out.println(3+4 + " my line");
 
 	}
 }
